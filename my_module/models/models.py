654b86ac392a5252fb6050867aa2f67c54a89419
esagents <odoo@eupd3.odoo.com>
2021-03-14 12:59:26 +0000

    [ADD] my_module: add the start_datetime field to the model my_module.my_module

---
+++ my_module/models/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class my_module(models.Model):
+    _name = 'my_module.my_module'
+    _description = 'my_module.my_module'
+
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+    start_datetime = fields.Datetime('Start time', default=lambda self: fields.Datetime.now())
+
+    @api.depends('value')
+    def _value_pc(self):
+         for record in self:
+           record.value2 = float(record.value) / 100
