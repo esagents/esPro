1f68f4306d4063b049fe83818d928f190c5dfbda
esagents.com <vu.nguyenlt@esagents.com>
2021-03-14 13:47:39 +0000

    [ADD] my_module: add the start_datetime field to the model my_module.my_module

--- my_module/models/.ipynb_checkpoints/models-checkpoint.py
+++ my_module/models/.ipynb_checkpoints/models-checkpoint.py
@@ -1,18 +1,30 @@
 # -*- coding: utf-8 -*-
+from pyodbc import pyodbc 
+from unidecode import unidecode
+from odoo import models, fields, api
 
-# from odoo import models, fields, api
+class my_module(models.Model):
+    _name = 'my_module.my_module'
+    _description = 'my_module.my_module'
 
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+    start_datetime = fields.Datetime('Start time', default=lambda self: fields.Datetime.now())
+    
+    @api.model
+    def create(self, values):
+        if 'name' in values:
+            values['name'] = unidecode(values['name'])
+        return super(my_module, self).create(values)
 
-# class my_module(models.Model):
-#     _name = 'my_module.my_module'
-#     _description = 'my_module.my_module'
+    def write(self, values):
+        if 'name' in values:
+            values['name'] = unidecode(values['name'])
+        return super(my_module, self).write(values)
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    @api.depends('value')
+    def _value_pc(self):
+         for record in self:
+           record.value2 = float(record.value) / 100
--- my_module/models/models.py
+++ my_module/models/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from pyodbc import pyodbc 
 from unidecode import unidecode
 from odoo import models, fields, api
 
